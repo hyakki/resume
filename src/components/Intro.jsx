d4442afc9950e273a3c36908a490b171d12e7244
hyakk1 <me@hyakki.io>
2021-04-24 08:01:35 +0200

    Quick fixes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,7 @@ class Intro extends React.Component {
         <h1>Maxime PARISSE <small>Senior Front-End Developer</small></h1>
         <p>
           Based on Evere (Bruxelles) - Belgium<br />
-          <a className="link" href="mailto:maximeparisse@gmail.com">maximeparisse@gmail.com</a><br />
+          <a className="link" href="mailto:me@hyakki.io">me@hyakki.io</a><br />
           <a className="link" href="tel:0032472555291">+32 (0) 472 555 291</a><br />
         </p>
       </>
